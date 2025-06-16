73ee61fef5ac6ab7486abfb7d05adf918192c4aa
TimHei123 <58216216+TimHei123@users.noreply.github.com>
2025-06-16 10:39:35 +0200

    swagger

--- backend/go/app/hypervisors.go
+++
@@ -1,9 +0,0 @@
-package hypervisors
-
-// HypervisorFunctions defines the interface for hypervisor operations
-type HypervisorFunctions interface {
-	ListAllVms() ([]byte, error)
-	ListVmsUser([]int, string) ([]map[string]interface{}, error)
-	CreateServer(string, int, int, int, string, string, string, string, string) (map[string]string, error)
-	DeleteServer(int, string) (string, error)
-}
